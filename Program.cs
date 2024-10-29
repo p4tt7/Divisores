0df89c8a5ce853e8ad5c5c6ec6830b56be3dc7a3
patty 💜 <87675545+p4tt7@users.noreply.github.com>
2024-10-29 19:39:47 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int x = int.Parse(Console.ReadLine());
+int y = int.Parse(Console.ReadLine());
+
+if (y!=0 && x%y==0){
+    Console.WriteLine("Es divisible!!");
+}
+
+else{
+    Console.WriteLine("No es divisible :(");
+}
+
